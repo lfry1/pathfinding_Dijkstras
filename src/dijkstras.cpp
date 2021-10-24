c488049584685dcb33b998c017ba5e61382828dd
lfry1 <89421026+lfry1@users.noreply.github.com>
2021-10-24 16:22:48 -0400

    only very minor changes
    mostly for clarity and testing, ultimate output is i think unaffected

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -41,16 +41,16 @@ public:
     {
         reverse = revparam;
     }
-    bool operator()(const Node &n1, const Node &n2) const
+    bool operator()(const Node *n1, const Node *n2) const
     {
         if (reverse)
-            return !((n1.row == n2.row) && (n1.col == n2.col));
+            return !((n1->row == n2->row) && (n1->col == n2->col));
         else
-            return ((n1.row == n2.row) && (n1.col == n2.col));
+            return ((n1->row == n2->row) && (n1->col == n2->col));
     }
 };
 
-typedef priority_queue<Node, vector<Node>, mycomparison> nodePQ;
+typedef priority_queue<Node *, vector<Node *>, mycomparison> nodePQ;
 
 // declaration for function used to implement Dijkstra's
 void shortestPath(Graph &G, int startRow, int startCol, int endRow, int endCol);
@@ -124,10 +124,9 @@ int main(int argc, char *argv[])
     startNode.row = startRow; // row and col are all you ned to compare Nodes
     startNode.col = startCol;
 
-    // nodePQ tilesByDistance; // shortestPath will fill this
     shortestPath(g, startRow, startCol, endRow, endCol);
-    // cout << "eh?" << endl;
-    print(g, startRow, startCol, endRow, endCol);
+    cout << "eh?" << endl;
+    // print(g, startRow, startCol, endRow, endCol);
     return 0;
 } // end main
 
@@ -137,13 +136,12 @@ void shortestPath(Graph &g, int startRow, int startCol, int endRow, int endCol)
     nodePQ unvisited;
     // nodePQ visited; // do we even need this for this application
     vector<Node> visited;
-    vector<Node> fVisited;
+    // vector<Node> fVisited;
 
     Node start;
-    Node end2;
-    Node *end;
-    // Node *nullDummy = new Node;
-    //  CREATE UNVISITED
+    int unvisitedCount = 0;
+
+    //   CREATE UNVISITED
     for (int i = 0; i < g.MAP_ROWS; i++)
     {
         for (int j = 0; j < g.MAP_COLS; j++)
@@ -151,79 +149,85 @@ void shortestPath(Graph &g, int startRow, int startCol, int endRow, int endCol)
             // change start node dist so it will be at top of unvisited
             if ((g.graph[i][j].row == startRow) && (g.graph[i][j].col == startCol))
             {
-                g.graph[i][j].distance = 0;
-                // g.graph[i][j].backedge = nullDummy;
-                g.graph[i][j].backRow = startRow - 1; // orleave as -1?
-                g.graph[i][j].backCol = startCol - 1;
+                g.graph[i][j].distance = 0;       // should ensure this is at top of unvisited
+                g.graph[i][j].backRow = startRow; // THESE ARE GETTING WRITTEN OVER L8R
+                g.graph[i][j].backCol = startCol;
             }
-            unvisited.push(g.graph[i][j]);
+            unvisited.push(&g.graph[i][j]);
+            unvisitedCount++;
         }
     }
+    cout << "added " << unvisitedCount << " elements to unvisited" << endl;
 
     // WHILE UNVISITED IS NOT EMPTY
     while (!unvisited.empty())
     {
-        Node n = unvisited.top(); // need to pop b4 next loop
+        Node *n = unvisited.top(); // need to pop b4 next loop
+        int curRow = n->row;
+        int curCol = n->col;
         nodePQ frontier;
 
         // FIND ADJACENCIES AND PUT THEM IN THE FRONTIER
-        if (n.row > 0) // up: i-1
+        if (curRow > 0) // up: i-1
         {
-            frontier.push(g.graph[n.row - 1][n.col]);
+            frontier.push(&g.graph[curRow - 1][curCol]);
         }
-        if (n.row < g.MAP_ROWS - 2) // down: i+1
+        if (curRow < g.MAP_ROWS - 2) // down: i+1
         {
-            frontier.push(g.graph[n.row + 1][n.col]);
+            frontier.push(&g.graph[curRow + 1][curCol]);
         }
-        if (n.col > 0) // left: j-1
+        if (curCol > 0) // left: j-1
         {
-            frontier.push(g.graph[n.row][n.col - 1]);
+            frontier.push(&g.graph[curRow][curCol - 1]);
         }
-        if (n.col < g.MAP_COLS - 2) // right: j+1
+        if (curCol < g.MAP_COLS - 2) // right: j+1
         {
-            frontier.push(g.graph[n.row][n.col + 1]);
+            frontier.push(&g.graph[curRow][curCol + 1]);
         }
 
         // FOR EACH ADJACENT NODE n2 ADJACENT TO n
         while (!frontier.empty())
         {
-            Node n2 = frontier.top(); // need to pop b4 next loop
+            Node *n2 = frontier.top(); // need to pop b4 next loop
+            int adjRow = n2->row;
+            int adjCol = n2->col;
             int thisDistance;
-            if (!n2.visited)
+
+            // SOMETHING IS WRONG INSIDE one of THESE LOOPS
+            if (!g.graph[adjRow][adjCol].visited)
             {
-                thisDistance = n.distance + n2.cost;
+                thisDistance = g.graph[curRow][curCol].distance + g.graph[adjRow][adjCol].cost;
                 // if you found a shorter path, update distance & backedge
-                if (n2.distance > thisDistance) // SIGN
+                if (g.graph[adjRow][adjCol].distance > thisDistance) // SIGN
                 {
-                    g.graph[n2.row][n2.col].distance = thisDistance;
-                    g.graph[n.row][n.col].backRow = n2.row;
-                    g.graph[n.row][n.col].backCol = n2.col;
+                    g.graph[adjRow][adjCol].distance = thisDistance; // used to be adjRow
+                    g.graph[curRow][curCol].backRow = adjRow;
+                    g.graph[curRow][curCol].backCol = adjCol;
+                    /*
+                    cout << "NODE:      " << curRow << " " << curCol << endl;
+                    cout << "BACKEDGE:  " << g.graph[curRow][curCol].backRow << " " << g.graph[curRow][curCol].backCol << endl;
+                    cout << endl;
+                    */
                 }
             }
             frontier.pop(); // pops n2
         }                   // end while(!frontier.empty())
-        n.visited = true;
-        visited.push_back(n);
+        g.graph[curRow][curCol].visited = true;
+        visited.push_back(g.graph[curRow][curCol]);
         unvisited.pop(); // pops n
     }                    // end while(!unvisited.empty())
-
-    /* //TESTING -  UNCOMMENT THIS TO SEE MORE PROBLEM DETAILS
-    visited.pop();
-    visited.pop();
-    Node top = visited.top();
-    cout << "at END of dijkstras I think top is " << top.row << " " << top.col << endl;
-    cout << "and I think top->backedge is  " << top.backedge->row << " " << top.backedge->col << endl;
-    cout << "at the END of dijkstras I think end2->backedge is " << end2.backedge->row << " " << end2.backedge->col << endl;
-
-
-    for (int i = 1; i < visited.size(); i++)
+    // TESTING
+    for (int i = 0; i < g.MAP_ROWS; i++)
     {
-        cout << "NODE FINAL:      " << visited[i].row << " " << visited[i].col << endl;
-        cout << "BACKEDGE FINAL:  " << g.graph[visited[i].row][visited[i].col].backRow << " " << g.graph[visited[i].row][visited[i].col].backCol << endl;
+        for (int j = 0; j < g.MAP_COLS; j++)
+        {
+            cout << "FINAL NODE:      " << g.graph[i][j].row << " " << g.graph[i][j].col << endl;
+            cout << "FINAL BACKEDGE:  " << g.graph[i][j].backRow << " " << g.graph[i][j].backCol << endl;
+            cout << endl;
+        }
     }
-    */
-} // end dijkstras
-
+    // END TESTING
+}
 //+++++++++++++++++++++++++ MISC UTILITY FNS ++++++++++++++++++++++++
 
 // print function takes END NODE as arg, prints path from START TO END
